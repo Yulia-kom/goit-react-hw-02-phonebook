25ae477f6456de19b24f40d93f62633f6a6db6b7
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-02-14 21:37:46 +0200

    hw1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+import React, { Component } from "react";
+//import { nanoid } from 'nanoid';
+import ContactList from "./ContactList";
+import contacts from "./contacts.json";
+
+class App extends Component {
+
+  state = {
+    contacts: contacts ,
+    filter: '',
+    name: '',
+    number: ''
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+   <div>
+  <h1>Phonebook</h1>
+  {/* <ContactForm  /> */}
+
+  <h2>Contacts</h2>
+  {/* <Filter  /> */}
+  <ContactList contacts={contacts} />
+</div>
   );
+  }
+  
 };
+
+export default App;
+
+
+// model.id = nanoid()
